c0e60447144004865006f0af3920801bcd095aed
Krantiditya .(krantiditya) <krantiditya@infosys.com>
2025-06-03 08:22:37 +0530

    execute database_script.sql when database is empty

--- src/app.py
+++ src/app.py
@@ -9,6 +9,7 @@ DBNAME = os.environ["DBNAME"]
 ENDPOINT = os.environ["ENDPOINT"]
 REGION_NAME = os.environ["REGION_NAME"]
 SECRET_NAME = os.environ["SECRET_NAME"]
+STATIC_WEBSITE_URL = os.environ["STATIC_WEBSITE_URL"]
 
 
 def get_secret():
@@ -24,6 +25,17 @@ def get_secret():
     secret = get_secret_value_response["SecretString"]
     secret = json.loads(secret)
     return secret
+
+
+def run_db_script(db, cursor, filename):
+    with open(filename, "r") as file:
+        sql_commands = file.read().split(";")
+        for command in sql_commands:
+            if command.strip():
+                print(command)
+                print("-------------------")
+                cursor.execute(command)
+    db.commit()
 
 
 def lambda_handler(event, context):
@@ -45,13 +57,32 @@ def lambda_handler(event, context):
 
         if db.is_connected():
             print("Connected to the database successfully!")
+            cursor = db.cursor()
+
+            no_of_tables_query = f"""SELECT count(*)
+                                    FROM information_schema.tables
+                                    WHERE table_schema = '{DBNAME}'"""
+            cursor.execute(no_of_tables_query)
+            tables = cursor.fetchall()
+            no_of_tables = tables[0][0]
+
+            if no_of_tables == 0:
+                print("Database is empty, executing database_script.sql ....")
+                run_db_script(db, cursor, "database_script.sql")
+                cursor.execute(no_of_tables_query)
+                tables = cursor.fetchall()
+                no_of_tables = tables[0][0]
+                print("database_script.sql executed successfully!")
+                print("no of tables in database are", no_of_tables)
+            else:
+                print("Tables in database are", tables)
+
             query = f"""select count(*), sum(amount) 
-                    from database01.Transaction 
+                    from {DBNAME}.Transaction 
                     where account_id in (select account_id 
-                                        from database01.Account
+                                        from {DBNAME}.Account
                                         where customer_id={customer_id})"""
 
-            cursor = db.cursor()
             cursor.execute(query)
             result = cursor.fetchall()
             result = {"count": result[0][0], "sum": float(result[0][1])}
@@ -61,7 +92,7 @@ def lambda_handler(event, context):
             return {
                 "statusCode": 200,
                 "headers": {
-                    "Access-Control-Allow-Origin": "http://static-website-hosting-bucket-ap-south-1-394504785337.s3-website.ap-south-1.amazonaws.com",
+                    "Access-Control-Allow-Origin": STATIC_WEBSITE_URL,
                     "Access-Control-Allow-Methods": "GET,OPTIONS",
                     "Access-Control-Allow-Headers": "Content-Type,Authorization,auth-token,customer_id",
                 },
