0180b234b7905234b094fc0e13964476579ba8d7
Krantiditya .(krantiditya) <krantiditya@infosys.com>
2025-06-03 10:47:38 +0530

    code refactor

--- src/app.py
+++ src/app.py
@@ -5,94 +5,113 @@ import boto3
 import mysql.connector
 from botocore.exceptions import ClientError
 
-DBNAME = os.environ["DBNAME"]
-ENDPOINT = os.environ["ENDPOINT"]
-REGION_NAME = os.environ["REGION_NAME"]
-SECRET_NAME = os.environ["SECRET_NAME"]
-STATIC_WEBSITE_URL = os.environ["STATIC_WEBSITE_URL"]
 
+class Database:
+    def __init__(self):
+        self.endpoint = os.environ["ENDPOINT"]
+        self.dbname = os.environ["DBNAME"]
+        self.region_name = os.environ["REGION_NAME"]
+        self.secret_name = os.environ["SECRET_NAME"]
+        self.static_website_url = os.environ["STATIC_WEBSITE_URL"]
 
-def get_secret():
+    def get_secret(self):
+        session = boto3.session.Session()
+        client = session.client(
+            service_name="secretsmanager", region_name=self.region_name
+        )
 
-    session = boto3.session.Session()
-    client = session.client(service_name="secretsmanager", region_name=REGION_NAME)
+        try:
+            get_secret_value_response = client.get_secret_value(
+                SecretId=self.secret_name
+            )
+        except ClientError as e:
+            raise e
 
-    try:
-        get_secret_value_response = client.get_secret_value(SecretId=SECRET_NAME)
-    except ClientError as e:
-        raise e
+        secret = get_secret_value_response["SecretString"]
+        secret = json.loads(secret)
+        return secret
 
-    secret = get_secret_value_response["SecretString"]
-    secret = json.loads(secret)
-    return secret
+    def connect_to_db(self):
+        secret = self.get_secret()
+        conn = mysql.connector.connect(
+            host=self.endpoint,
+            database=self.dbname,
+            user=secret["username"],
+            password=secret["password"],
+        )
+        return conn
 
+    def run_db_script(self, db, cursor, filename):
+        with open(filename, "r") as file:
+            sql_commands = file.read().split(";")
+            for command in sql_commands:
+                if command.strip():
+                    print(command)
+                    print("-------------------")
+                    cursor.execute(command)
+        db.commit()
 
-def run_db_script(db, cursor, filename):
-    with open(filename, "r") as file:
-        sql_commands = file.read().split(";")
-        for command in sql_commands:
-            if command.strip():
-                print(command)
-                print("-------------------")
-                cursor.execute(command)
-    db.commit()
+    def get_table_count(self, cursor):
+        no_of_tables_query = f"""SELECT count(*)
+                                FROM information_schema.tables
+                                WHERE table_schema = '{self.dbname}'"""
+        cursor.execute(no_of_tables_query)
+        tables = cursor.fetchall()
+        no_of_tables = tables[0][0]
+        return no_of_tables
+
+    def initialize_database(self, db, cursor):
+        no_of_tables = self.get_table_count(cursor)
+        if no_of_tables == 0:
+            print("Database is empty, executing database_script.sql ....")
+            self.run_db_script(db, cursor, "database_script.sql")
+            no_of_tables = self.get_table_count(cursor)
+            print("database_script.sql executed successfully!")
+            print("no of tables in database are", no_of_tables)
+        else:
+            print("Tables in database are", no_of_tables)
+
+    def get_transaction_data(self, customer_id):
+        db = self.connect_to_db()
+        cursor = db.cursor()
+        query = f"""select count(*), sum(amount)
+                from {self.dbname}.Transaction
+                where account_id in (select account_id
+                                    from {self.dbname}.Account
+                                    where customer_id={customer_id})"""
+
+        cursor.execute(query)
+        result = cursor.fetchall()
+        result = {"count": result[0][0], "sum": float(result[0][1])}
+        return result
+
+    def get_static_website_url(self):
+        return self.static_website_url
 
 
 def lambda_handler(event, context):
 
     print("event: ", event)
-
-    headers = event["headers"]
-    customer_id = headers["customer_id"]
-
-    secret = get_secret()
-
     try:
-        db = mysql.connector.connect(
-            host=ENDPOINT,
-            user=secret["username"],
-            password=secret["password"],
-            database=DBNAME,
-        )
+        headers = event["headers"]
+        customer_id = headers["customer_id"]
 
+        db_obj = Database()
+        db = db_obj.connect_to_db()
         if db.is_connected():
             print("Connected to the database successfully!")
+
             cursor = db.cursor()
-
-            no_of_tables_query = f"""SELECT count(*)
-                                    FROM information_schema.tables
-                                    WHERE table_schema = '{DBNAME}'"""
-            cursor.execute(no_of_tables_query)
-            tables = cursor.fetchall()
-            no_of_tables = tables[0][0]
-
-            if no_of_tables == 0:
-                print("Database is empty, executing database_script.sql ....")
-                run_db_script(db, cursor, "database_script.sql")
-                cursor.execute(no_of_tables_query)
-                tables = cursor.fetchall()
-                no_of_tables = tables[0][0]
-                print("database_script.sql executed successfully!")
-                print("no of tables in database are", no_of_tables)
-            else:
-                print("Tables in database are", tables)
-
-            query = f"""select count(*), sum(amount) 
-                    from {DBNAME}.Transaction 
-                    where account_id in (select account_id 
-                                        from {DBNAME}.Account
-                                        where customer_id={customer_id})"""
-
-            cursor.execute(query)
-            result = cursor.fetchall()
-            result = {"count": result[0][0], "sum": float(result[0][1])}
-
+            db_obj.initialize_database(db, cursor)
+            result = db_obj.get_transaction_data(customer_id)
             db.close()
+
+            static_website_url = db_obj.get_static_website_url()
 
             return {
                 "statusCode": 200,
                 "headers": {
-                    "Access-Control-Allow-Origin": STATIC_WEBSITE_URL,
+                    "Access-Control-Allow-Origin": static_website_url,
                     "Access-Control-Allow-Methods": "GET,OPTIONS",
                     "Access-Control-Allow-Headers": "Content-Type,Authorization,auth-token,customer_id",
                 },
